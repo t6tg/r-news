e545e42c9a1be9706554db1f78f9c300de7ba87d
thanawatgulati <thanawatgulatijames@gmail.com>
2020-07-03 01:09:09 +0700

    config tailwind css

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import "../../static/main.css"
+
+interface Props {}
+
+const Header = (props: Props) => {
+  return <div></div>
+}
+
+export default Header
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,13 @@
 import React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
+interface Props {}
+
+const index = (props: Props) => {
+  return (
+    <div>
+      <p className="text-2xl">Hello World</p>
+    </div>
+  )
 }
+
+export default index
