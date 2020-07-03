1f41e639f36d0100e8ef0fe7f9e6a3cbabdb09dd
thanawatgulati <thanawatgulatijames@gmail.com>
2020-07-03 15:48:50 +0700

    purpose: maskable

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,11 +29,13 @@ module.exports = {
             src: `/favicons/android-chrome-192x192.png`,
             sizes: `192x192`,
             type: `image/png`,
+            purpose: `maskable`,
           },
           {
             src: `/favicons/android-chrome-512x512.png`,
             sizes: `512x512`,
             type: `image/png`,
+            purpose: `maskable`,
           },
         ],
       },
