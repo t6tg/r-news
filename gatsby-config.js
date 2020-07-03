652a4407f7c2cf324e43e8a53057c95f1b2137e1
thanawatgulati <thanawatgulatijames@gmail.com>
2020-07-03 13:45:01 +0700

    Config PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,5 +12,20 @@ module.exports = {
     url: "https://www.redxav.com", // No trailing slash allowed!
     image: "/images/snape.jpg", // Path to your image you placed in the 'static' folder
   },
-  plugins: [`gatsby-plugin-postcss`],
+  plugins: [
+    `gatsby-plugin-postcss`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `RedXAV`,
+        short_name: `RedXAV`,
+        start_url: `/`,
+        background_color: `#1A202C`,
+        theme_color: `#9B0C19`,
+        display: `standalone`,
+        icon: `static/images/icon.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
 }
