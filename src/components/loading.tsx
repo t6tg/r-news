646fe32b3acf32cf5f7e7fd9b16c44cdbb7ef2f5
thanawatgulati <thanawatgulatijames@gmail.com>
2020-07-04 14:10:01 +0700

    fix performance

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -7,17 +7,17 @@ interface Props {
 const Load = (props: Props) => {
   if (props.status === "load") {
     return (
-      <div className="bg-green-700 bg-opacity-25 border-4 border-green-800 p-2 rounded-full font-bold">
+      <main className="bg-green-700 bg-opacity-25 border-4 border-green-800 p-2 rounded-full font-bold">
         <span className="bg-green-400 p-1 px-2 rounded-full">Status</span>
         <span className="px-2">Loading . . . </span>
-      </div>
+      </main>
     )
   } else if (props.status === "error") {
     return (
-      <div className="bg-red-700 bg-opacity-25 border-4 border-red-800 p-2 rounded-full font-bold">
+      <main className="bg-red-700 bg-opacity-25 border-4 border-red-800 p-2 rounded-full font-bold">
         <span className="bg-red-400 p-1 px-2 rounded-full">Status</span>
         <span className="px-2">Error , Please try again later.</span>
-      </div>
+      </main>
     )
   }
 }
