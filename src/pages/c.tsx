8b49c5be0b9023f631bafba90c893fd16b53ab9e
thanawatgulati <thanawatgulatijames@gmail.com>
2020-07-04 12:25:54 +0700

    change Lightmode color

--- src/pages/c.tsx
+++ src/pages/c.tsx
@@ -1,11 +1,13 @@
 import React from "react"
 import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 interface Props {}
 
 const c = (props: Props) => {
   return (
     <Layout>
+      <SEO title="Categories" />
       <div></div>
     </Layout>
   )
